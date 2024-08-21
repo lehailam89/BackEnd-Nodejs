1d39269178bb81e17a5695768472332061ef50f5
Lâm <lehailamltk@gmail.com>
2024-08-21 13:33:10 +0700

    update 21/8/2024

--- product-management/controllers/client/product.controller.js
+++ product-management/controllers/client/product.controller.js
@@ -5,10 +5,7 @@ module.exports.index = async (req, res) => {
     const products = await Product.find({
         deleted: false
     }).sort({ position: "desc"});
-
-    
-    
-    console.log(products);
+ 
 
     res.render("client/pages/products/index.pug", {
         pageTitle: "Danh sách sản phẩm",
--- product-management/index.js
+++ product-management/index.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 var flash = require("express-flash");
-const multer = require('multer');
 
 require("dotenv").config();
 
