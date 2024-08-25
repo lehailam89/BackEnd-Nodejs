23c836889546a52ba9bd3479e0d980eb60e8e145
Lâm <lehailamltk@gmail.com>
2024-08-25 15:03:00 +0700

    update upload day file len cloud

--- product-management/middlewares/uploadCloud.middleware.js
+++ product-management/middlewares/uploadCloud.middleware.js
@@ -3,8 +3,8 @@ const streamifier = require('streamifier')
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
+    api_key: process.env.CLOUD_KEY,
+    api_secret: process.env.CLOUD_SECRET
 })
 
 module.exports.upload = (req, res, next) => {
