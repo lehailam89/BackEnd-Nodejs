d585cfc3b637f9153bb3429b050abfa3aad5236a
Lâm <lehailamltk@gmail.com>
2024-09-26 17:22:55 +0700

    cap nhat so luong san pham trong gio hang

--- product-management/controllers/client/cart.controller.js
+++ product-management/controllers/client/cart.controller.js
@@ -97,3 +97,27 @@ module.exports.delete = async (req, res) => {
     req.flash("success", "Xóa sản phẩm khỏi giỏ hàng thành công");
     res.redirect('back');
 }
+
+//[GET] /cart/update/:productId/:quantity
+module.exports.update = async (req, res) => {
+    const cartId = req.cookies.cartId;
+    const productId = req.params.productId;
+    const quantity = req.params.quantity;
+
+    await Cart.updateOne(
+        {
+            _id: cartId,
+            "products.product_id": productId
+        },
+        {
+            $set: {
+                "products.$.quantity": quantity
+            }
+        }
+    );
+
+    req.flash("success", "Cập nhật giỏ hàng thành công");
+
+    res.redirect("back");
+
+}
