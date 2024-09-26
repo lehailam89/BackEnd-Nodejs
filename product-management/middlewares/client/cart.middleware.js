740ad58bc415484656c3f0e56c848935a42cbb6a
Lâm <lehailamltk@gmail.com>
2024-09-26 09:41:23 +0700

    lam mini cart tren header

--- product-management/middlewares/client/cart.middleware.js
+++ product-management/middlewares/client/cart.middleware.js
@@ -7,12 +7,18 @@ module.exports.cartId = async (req, res, next) => {
 
         const expiresTime = 1000 * 60 * 60 * 24 * 365;
     
-        console.log(cart);
+        console.log(cart); 
         res.cookie("cartId", cart.id, {
             expires: new Date(Date.now() + expiresTime),
         });
     }else{
+        const cart = await Cart.findOne({
+            _id: req.cookies.cartId 
+        })
 
+        cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
+
+        res.locals.miniCart = cart;
     }
     next();
 }
