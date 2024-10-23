009a0fa4942c49ba326d2c8a9120c57b75cb380a
Lâm <lehailamltk@gmail.com>
2024-10-23 09:08:14 +0700

    add page 404 va sua loi middleware cart

--- product-management/middlewares/client/cart.middleware.js
+++ product-management/middlewares/client/cart.middleware.js
@@ -1,24 +1,31 @@
-const Cart = require('../../models/cart.model');
+const Cart = require('../../models/cart.model.js')
 
 module.exports.cartId = async (req, res, next) => {
+
+  try {
     if (!req.cookies.cartId) {
-        const cart = new Cart();
-        await cart.save();
+      const cart = await Cart.create({})
+  
+      const expiresTime = 1000 * 60 * 60 * 24 * 365;
+  
+      res.cookie("cartId", cart.id, {
+        expires: new Date(Date.now() + expiresTime)
+      })
+    } else {
+      const cart = await Cart.findById(req.cookies.cartId).lean()
+      if (!cart) {
+        res.clearCookie("cartId")
+        return res.redirect('back')
+      }
 
-        const expiresTime = 1000 * 60 * 60 * 24 * 365;
-    
-        console.log(cart); 
-        res.cookie("cartId", cart.id, {
-            expires: new Date(Date.now() + expiresTime),
-        });
-    }else{
-        const cart = await Cart.findOne({
-            _id: req.cookies.cartId 
-        })
+      cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0)
 
-        cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0);
-
-        res.locals.miniCart = cart;
+      res.locals.miniCart = cart
     }
-    next();
+    next()
+  } catch (error) {
+    res.clearCookie("cartId")
+    return res.redirect('back')
+  }
+
 }
