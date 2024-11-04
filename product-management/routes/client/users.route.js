28857c6526d01aaea07bd3f97034a9dd9e19451c
Lâm <lehailamltk@gmail.com>
2024-11-04 17:16:09 +0700

    hien thi danh sach user

---
+++ product-management/routes/client/users.route.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require('../../controllers/client/users.controller.js');
+
+router.get('/not-friend', controller.notFriend);
+
+module.exports = router;
